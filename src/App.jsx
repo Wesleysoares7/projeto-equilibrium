02d1fe00eeabf43626cdd31c6efc68f368870984
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-05-21 15:28:19 -0300

    feat: criação da seção hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
+import Button from "./components/button/button";
 import Header from "./components/header/header";
+import Hero from "./components/hero/hero";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,27 @@
+import Button from "../button/button";
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <section id="hero">
+      <div className="hero">
+        <img
+          src="./public/images/hero.jpg"
+          alt="imagem de tres pessoas fazendo exercicio de pilates"
+        />
+        <div className="hero-text">
+          <h1>Instituto Equilibrium</h1>
+          <h3>Estudio de Pilates</h3>
+          <p>
+            Conheça os beneficios da prática do Pilates para o seu dia a dia!
+          </p>
+          <div className="hero-button">
+            <Button />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
