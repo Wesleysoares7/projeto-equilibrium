d9a976ba3b28c76f9d0a50756ce04fc41c315fd4
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-05-21 15:27:10 -0300

    fix: transposição do menu mobile

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./header.css";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+  const toggleRef = useRef(null);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const handleClickOutside = (event) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target) &&
+      toggleRef.current &&
+      !toggleRef.current.contains(event.target)
+    ) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isMenuOpen]);
 
   return (
     <section>
@@ -16,6 +40,7 @@ const Header = () => {
             <img src="./public/images/logo.png" alt="Logo" />
           </div>
           <div
+            ref={toggleRef}
             className={`menu-toggle ${isMenuOpen ? "open" : ""}`}
             onClick={toggleMenu}
           >
@@ -23,9 +48,9 @@ const Header = () => {
             <span></span>
             <span></span>
           </div>
-          <ul className={`menu ${isMenuOpen ? "open" : ""}`}>
+          <ul ref={menuRef} className={`menu ${isMenuOpen ? "open" : ""}`}>
             <li>
-              <a href="#Home">Home</a>
+              <a href="#Hero">Home</a>
             </li>
             <li>
               <a href="#Sobre">Sobre</a>
